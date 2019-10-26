837dd204d0d2c1149bcca7eeb0cadb1cde962899
M.Hamza Ali <hamzaali44@gmail.com>
2019-10-26 03:37:28 -0500

    fixed resend order, added some modularity, added logs

--- src/client.java
+++ src/client.java
@@ -1,16 +1,8 @@
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 
 public class client {
@@ -19,7 +11,7 @@ public class client {
 	private static final int MAXREADSIZE = 30;
 	private static final int EXTRAPACKETS = 0;
 	private static final int WINDOWSIZE = 7;
-	private static final int TIMER = 50;
+	private static final int TIMER = 500;
 	private static final String seqlog = "clientseqnum.log";
 	private static final String acklog = "clientack.log";
 	
@@ -39,87 +31,10 @@ public class client {
 		
 		clientSocket.close();
 	}
-	
-	private static void writeFile(String filename, String filecontent) throws IOException
-	{
-		FileWriter fw = new FileWriter(filename,false);//can also buffer the output to the writing buffer so that all of the file is not held in the memory
-		fw.write(filecontent);
-		fw.close();
-	}
-	
-	private static byte[] serializePacket(packet data) throws IOException {
-		ByteArrayOutputStream ostream = new ByteArrayOutputStream();
-		ObjectOutputStream oostream = new ObjectOutputStream(ostream);
-		
-		oostream.writeObject(data);
-		oostream.close();
-		
-		byte[] toReturn = ostream.toByteArray();
-		ostream.close();
-		
-		return toReturn;
-	}
-	
-	private static packet deserializePacket(byte[] dataToDeserialize) throws IOException, ClassNotFoundException {
-		ByteArrayInputStream iStream = new ByteArrayInputStream(dataToDeserialize);
-		ObjectInputStream oiStream = new ObjectInputStream(iStream);
-		
-		packet toReturn = (packet) oiStream.readObject();
-		
-		iStream.close();
-		oiStream.close();
-		
-		return toReturn;
-	}
-	
-	private static byte[][] serializePacketizedFile(String fileName) throws IOException
-	{
-		packet[] packets = packetizeFile(fileName);
-		byte[][] toReturn = new byte[packets.length][];
-		
-		for(int i=0;i<packets.length;i++)
-		{
-			toReturn[i] = serializePacket(packets[i]);
-		}
-		
-		return toReturn;
-	}
-	
-	private static packet[] packetizeFile(String fileName) throws IOException{
-		
-		String fileContent = readFile(fileName);//can also read file in chunks of 30 characters if the file is too big
-		packet[] packets = new packet[fileContent.length()/MAXREADSIZE + 1 + EXTRAPACKETS];
-		int ind=0;
-		packets[ind] = new packet(1,0,0,null);
-		for(int i=0;i<fileContent.length();i+=MAXREADSIZE)
-		{
-			ind = i/MAXREADSIZE;
-			try
-			{
-				packets[ind] = new packet(1,ind%(WINDOWSIZE+1),MAXREADSIZE,fileContent.substring(i, i+MAXREADSIZE));
-			}catch(IndexOutOfBoundsException e)
-			{
-				packets[ind] = new packet(1,ind%(WINDOWSIZE+1),fileContent.length()-i, fileContent.substring(i, fileContent.length()));
-				break;
-			}
-			System.out.println(packets[ind].getData());
-		}
-		/*ind += 1;
-		
-		packets[ind] = new packet(3,ind%7,0,null);*/
-		
-		return packets;
-	}
-	
-	private static String readFile(String fileName) throws IOException {
-		String content = new String(Files.readAllBytes(Paths.get(fileName)),StandardCharsets.US_ASCII);
-		return content;
-	}
 
 	private static void talkOn(DatagramSocket clientSocket, String serverName, int serverPort, String fileName) throws IOException, ClassNotFoundException {
-		//byte[] serializedData = serializePacket(data);
 		
-		byte[][] serializedPackets = serializePacketizedFile(fileName);
+		byte[][] serializedPackets = commons.serializePacketizedFile(fileName,MAXREADSIZE,EXTRAPACKETS,WINDOWSIZE);
 		InetAddress serverHost = InetAddress.getByName(serverName);
 		
 		gbnTransfer(serializedPackets,clientSocket,serverHost,serverPort);
@@ -127,17 +42,15 @@ public class client {
 
 	private static void loadWindow(DatagramSocket clientSocket, LinkedList<DatagramPacket> window,InetAddress serverName, int serverPort, byte[][] serializedPackets, int base) throws IOException, ClassNotFoundException {
 		
-		
 		int n = WINDOWSIZE-window.size();
 		int s = window.size();
-		//System.out.println("start "+ base + " " + n + " " + min + " " + serializedPackets.length);
 		for(int i=0;i<n;i++)
 		{
 			try 
 			{
 				DatagramPacket item = new DatagramPacket(serializedPackets[base+s+i],serializedPackets[base+s+i].length,serverName,serverPort);
-				window.push(item);
-				sendAndLog(clientSocket,item,(base+s+i)%(WINDOWSIZE+1));
+				window.add(item);
+				commons.sendAndLog(clientSocket,item,seqlogbuff,(base+s+i)%(WINDOWSIZE+1));
 			}catch(IndexOutOfBoundsException e)
 			{
 				break;
@@ -145,12 +58,6 @@ public class client {
 			
 		}
 		
-	}
-	
-	private static void sendAndLog(DatagramSocket clientSocket,DatagramPacket item,int seqno) throws IOException
-	{
-		clientSocket.send(item);
-		seqlogbuff.append(seqno + "\n");
 	}
 	
 	private static void gbnTransfer(byte[][] serializedPackets, DatagramSocket clientSocket, InetAddress serverName, int serverPort) throws IOException, ClassNotFoundException {
@@ -178,8 +85,7 @@ public class client {
 					
 					clientSocket.receive(ackPack);
 					
-					ack = deserializePacket(dataReceived);
-					
+					ack = commons.deserializePacket(dataReceived);
 					ackno = ack.getSeqNum();
 					acklogbuff.append(ackno + "\n");
 					if(ackno==nextSeq)
@@ -202,32 +108,32 @@ public class client {
 				
 			}catch(SocketTimeoutException e)
 			{
+				seqlogbuff.append("timeout here \n");
 				for(int i=0;i<window.size();i++)
 				{
 					DatagramPacket item = window.get(i);
-					sendAndLog(clientSocket,item,(base+i)%(WINDOWSIZE+1));
+					commons.sendAndLog(clientSocket,item,seqlogbuff,(base+i)%(WINDOWSIZE+1));
 				}
 			}
-			System.out.println(base+" "+serializedPackets.length);
 			
 		}
-
 		
-		byte[] EOTSerialized = serializePacket(new packet(3,nextSeq,0,null));
+		int last = base%(WINDOWSIZE+1);
+		byte[] EOTSerialized = commons.serializePacket(new packet(3,last,0,null));
 		DatagramPacket packetToSend = new DatagramPacket(EOTSerialized,EOTSerialized.length,serverName,serverPort);
-		sendAndLog(clientSocket,packetToSend,nextSeq);
+		commons.sendAndLog(clientSocket,packetToSend,seqlogbuff,last);
 		
 		byte[] dataToDeserialize = new byte[MAXBUFFERLEN];
 		DatagramPacket packetToReceive = new DatagramPacket(dataToDeserialize,dataToDeserialize.length);
 		clientSocket.setSoTimeout(0);
 		clientSocket.receive(packetToReceive);
 		
-		packet ack = deserializePacket(dataToDeserialize);
+		packet ack = commons.deserializePacket(dataToDeserialize);
 		int ackno = ack.getSeqNum();
 		acklogbuff.append(ackno + "\n");
 		
-		writeFile(seqlog,seqlogbuff.toString());
-		writeFile(acklog,acklogbuff.toString());
+		commons.writeFile(seqlog,seqlogbuff.toString());
+		commons.writeFile(acklog,acklogbuff.toString());
 		
 	}
 }
---
+++ src/commons.java
@@ -0,0 +1,92 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class commons {
+	
+	public static void writeFile(String filename, String filecontent) throws IOException
+	{
+		FileWriter fw = new FileWriter(filename,false);//can also buffer the output to the writing buffer so that all of the file is not held in the memory
+		fw.write(filecontent);
+		fw.close();
+	}
+
+	public static byte[] serializePacket(packet data) throws IOException {
+		ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+		ObjectOutputStream oostream = new ObjectOutputStream(ostream);
+		
+		oostream.writeObject(data);
+		oostream.close();
+		
+		byte[] toReturn = ostream.toByteArray();
+		ostream.close();
+		
+		return toReturn;
+	}
+	
+	public static packet deserializePacket(byte[] dataToDeserialize) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream iStream = new ByteArrayInputStream(dataToDeserialize);
+		ObjectInputStream oiStream = new ObjectInputStream(iStream);
+		
+		packet toReturn = (packet) oiStream.readObject();
+		
+		iStream.close();
+		oiStream.close();
+		
+		return toReturn;
+	}
+	
+	public static byte[][] serializePacketizedFile(String fileName, int maxreadsize,int extrapackets,int windowsize) throws IOException
+	{
+		packet[] packets = packetizeFile(fileName,maxreadsize,extrapackets,windowsize);
+		byte[][] toReturn = new byte[packets.length][];
+		
+		for(int i=0;i<packets.length;i++)
+		{
+			toReturn[i] = serializePacket(packets[i]);
+		}
+		
+		return toReturn;
+	}
+	
+	public static packet[] packetizeFile(String fileName, int maxreadsize,int extrapackets,int windowsize) throws IOException{
+		
+		String fileContent = readFile(fileName);//can also read file in chunks of 30 characters if the file is too big
+		packet[] packets = new packet[fileContent.length()/maxreadsize + 1 + extrapackets];
+		int ind=0;
+		packets[ind] = new packet(1,0,0,null);
+		for(int i=0;i<fileContent.length();i+=maxreadsize)
+		{
+			ind = i/maxreadsize;
+			try
+			{
+				packets[ind] = new packet(1,ind%(windowsize+1),maxreadsize,fileContent.substring(i, i+maxreadsize));
+			}catch(IndexOutOfBoundsException e)
+			{
+				packets[ind] = new packet(1,ind%(windowsize+1),fileContent.length()-i, fileContent.substring(i, fileContent.length()));
+				break;
+			}
+		}
+		
+		return packets;
+	}
+	
+	public static String readFile(String fileName) throws IOException {
+		String content = new String(Files.readAllBytes(Paths.get(fileName)),StandardCharsets.US_ASCII);
+		return content;
+	}
+	
+	public static void sendAndLog(DatagramSocket clientSocket,DatagramPacket item,StringBuilder seqlogbuff,int seqno) throws IOException
+	{
+		clientSocket.send(item);
+		seqlogbuff.append(seqno + "\n");
+	}
+}
